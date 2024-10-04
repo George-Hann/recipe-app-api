99fedbff4afbd5e2599b3e19036e859bcf8ae88d
George-Hann <george.hann@carbonresponsible.com>
2024-10-04 11:21:47 +0100

    Added user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -26,7 +26,7 @@ class PublicUserApiTests(TestCase):
     def test_create_user_success(self):
         """Test createing a user is successful"""
         payload = {
-            'email:': 'test@example.com',
+            'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'Test Name',
         }
@@ -40,25 +40,25 @@ class PublicUserApiTests(TestCase):
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
         payload = {
-            'email:': 'test@example.com',
+            'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'Test Name',
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.asserEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_too_short_error(self):
         """Test an error is returned if password less than 5 chars."""
         payload = {
-            'email:': 'test@example.com',
+            'email': 'test@example.com',
             'password': 'pw',
             'name': 'Test Name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.asserEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
